f4351275f2794bfc140ce09745ea80fd52c03c8a
MRensen <markrensen@hotmail.com>
2023-12-10 11:12:07 +0100

    dag 7 - deel 2 - finished

--- src/main/java/nl/markrensen/aoc/days/Day07.java
+++ src/main/java/nl/markrensen/aoc/days/Day07.java
@@ -7,6 +7,7 @@ import java.util.List;
 
 public class Day07 implements Day<Long> {
     List<Character> cards = new ArrayList<>(List.of('2','3','4','5','6','7','8','9','T','J','Q','K','A'));
+    List<Character> cards2 = new ArrayList<>(List.of('J','2','3','4','5','6','7','8','9','T','Q','K','A'));
 
     @Override
     public Long part1(List<String> input) {
@@ -41,6 +42,9 @@ public class Day07 implements Day<Long> {
             Hand h = hands.get(i);
             h.bidReturn = (long) h.bid * (i+1);
             result += h.bidReturn;
+            if(h.type == 3){
+                System.out.println();
+            }
         }
 
     return result;
@@ -55,20 +59,53 @@ public class Day07 implements Day<Long> {
 
         public Hand(Integer[] cardvalues, int bid) {
             this.cardvalues = cardvalues;
-            this.cardString = getCardString(cardvalues);
+            this.cardString = getCardString(cardvalues, false);
             this.bid = bid;
-            this.type = getType(cardvalues);
+            this.type = getType(cardvalues, false);
+        }
+        public Hand(Integer[] cardvalues, int bid, boolean part2){
+            this.cardvalues = cardvalues;
+            this.cardString = getCardString(cardvalues, true);
+            this.bid = bid;
+            int amountJ = 0;
+            for(char s : cardString.toCharArray()){
+                if(s == 'J'){
+                    amountJ++;
+                }
+            }
+            if(amountJ > 0){
+                List<Integer> types = new ArrayList<>();
+                for(int index = 0; index < cards2.size(); index++) {
+                    Integer[] newCardValue = new Integer[cardvalues.length];
+                    for (int i = 0; i < cardvalues.length; i++) {
+                        if (cardvalues[i] == 0) {
+                            newCardValue[i] = index;
+                        } else {
+                            newCardValue[i] = cardvalues[i];
+                        }
+                    }
+                    types.add(getType(newCardValue, true));
+                }
+                types.sort(Integer::compareTo);
+                this.type = types.get(types.size()-1);
+            } else {
+                this.type = getType(cardvalues, true);
+            }
         }
 
-        private String getCardString(Integer[] cardvalues){
+        private String getCardString(Integer[] cardvalues, boolean part2){
             StringBuilder sb = new StringBuilder();
             for(int i : cardvalues){
-                sb.append(cards.get(i));
+                if(part2){
+                    sb.append(cards2.get(i));
+                }else {
+                    sb.append(cards.get(i));
+                }
             }
             return sb.toString();
         }
 
-        private Integer getType(Integer[] cardvalues){
+        private Integer getType(Integer[] cardvalues, boolean part2){
             List<Integer> valueList = new ArrayList<>();
              Integer setValueOne = null;
              Integer setValueTwo = null;
@@ -111,7 +148,7 @@ public class Day07 implements Day<Long> {
                 return 5;
             }
             if(setAmountTwo != null) {
-                if ((setValueOne.equals(2) && setAmountTwo.equals(3)) || (setAmountOne.equals(3) && setAmountTwo.equals(2))) {
+                if ((setAmountOne.equals(2) && setAmountTwo.equals(3)) || (setAmountOne.equals(3) && setAmountTwo.equals(2))) {
                     // full house
                     return 4;
                 }
@@ -147,9 +184,45 @@ public class Day07 implements Day<Long> {
 
     @Override
     public Long part2(List<String> input) {
+        List<Hand> hands = new ArrayList<>();
+        for(String s : input){
+            String[] play = s.split(" ");
+            char[] handChars = play[0].trim().toCharArray();
+            Integer bid = Integer.parseInt(play[1].trim());
+            Integer[] hand = new Integer[play[0].length()];
+            for(int i = 0; i < handChars.length; i++){
+                hand[i] = cards2.indexOf(handChars[i]);
+            }
+            hands.add(new Hand(hand, bid, true));
+        }
+        hands.sort((h1, h2)->{
+            if(h1.type.equals(h2.type)){
 
-       return null;
+                for(int i = 0; i < h1.cardvalues.length; i++) {
+                    if (!h1.cardvalues[i].equals(h2.cardvalues[i])) {
+                        return h1.cardvalues[i].compareTo(h2.cardvalues[i]);
+                    }
+                }
+            } else {
+                return h1.type.compareTo(h2.type);
+            }
+            return 0;
+        });
+
+        long result = 0L;
+
+        for(int i = 0; i < hands.size(); i++){
+            Hand h = hands.get(i);
+            h.bidReturn = (long) h.bid * (i+1);
+            result += h.bidReturn;
+            if(h.type == 3){
+                System.out.println();
+            }
+        }
+
+        return result;
     }
+
 
 
 }
